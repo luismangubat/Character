da801b139cb0b6e63b9ff85a27ef5125c25634ba
luismangubat <Luis@Luiss-MacBook-Pro.local>
2015-10-20 12:51:43 -0700

    Initial check-in

---
+++ src/character/Character.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package character;
+
+import environment.ApplicationStarter;
+
+/**
+ *
+ * @author Luis
+ */
+public class Character {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationStarter.run("Dark Matter", new CharacterEnviroment());
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/character/CharacterEnviroment.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package character;
+
+import environment.Environment;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author Luis
+ */
+class CharacterEnviroment extends Environment {
+
+    public CharacterEnviroment() {
+    }
+
+    @Override
+    public void initializeEnvironment() {
+
+    }
+
+    @Override
+    public void timerTaskHandler() {
+    }
+
+    @Override
+    public void keyPressedHandler(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleasedHandler(KeyEvent e) {
+    }
+
+    @Override
+    public void environmentMouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void paintEnvironment(Graphics graphics) {
+    }
+
+}
